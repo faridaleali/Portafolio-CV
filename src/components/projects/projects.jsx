6fa0881dded3148eb225478cdc5c4c5ddcf9c34b
faridaleali <faleali.contact@gmail.com>
2023-09-16 05:07:21 -0300

    Mas cambios en la etiqueta a

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -42,7 +42,7 @@ export default function Projects() {
                     </div>
                   </div>
                   <div className="flex justify-center mt-7">
-                    <a href="cheers-pollofrito.netlify.app" target="_blank" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <a href="https://cheers-pollofrito.netlify.app" target="_blank" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Ir a la pagina
                         <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
